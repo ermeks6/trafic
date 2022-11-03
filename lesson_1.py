fe2bfc4485bdec939dbe0852c5f33d1477bf12b0
ermeks6 <ermek.maks10@gmail.com>
2022-11-04 01:18:14 +0600

    first working with "if operator"

---
+++ lesson_1.py
@@ -0,0 +1,21 @@
+a = "green"
+b = "yellow"
+c = "red"
+
+
+user = str(input("enter color:"))
+
+if user == a:
+    print("Go!")
+
+elif user == b:
+    print("Wait..")
+
+elif user == c:
+    print("STOP!!!")
+
+else:
+    print("go away")
+
+
+
